24362d20c92afd67100fe1acbe1d84f418f95cf1
Vinícius Noda <viniciusnoda@alunos.utfpr.edu.br>
2017-08-16 12:59:22 -0300

    ajustes finais a main

--- TSP/main.cpp
+++ TSP/main.cpp
@@ -14,31 +14,41 @@ using std::vector;
 const int CTRL{50};
 
 bool end(vector<Tour>&);
-
 bool interv(double&,double&);
+void mutate(vector<Tour>&);
+void crossPop(vector<Tour>&);
 
 int main(){
     srand((unsigned)time(0));
     int tam_pop,tam_map;
 
     vector<Tour> pop;
+
+    cout<<"Digite o tamanho da populacao e a quantidade de cidades: ";
     cin >> tam_pop >> tam_map;
 
+    //cria o mapa
     Map objMap(tam_map);
 
+    //gera a primeira população 
     for(int i=0; i<tam_pop; i++){
         Tour objT(objMap);
         pop.push_back(objT);
     }
 
-    while(){
+    while(end(pop)){
+        //seleciona a proxima pop
 
+        //realiza o crossover
+        crossPop(pop);
+        //realiza a mutação
+        mutate(pop);
     }
 
     return(0);
 }
 
-bool end(vector<Tour> t){
+bool end(vector<Tour> &t){
     //conta quantas gerações se passaram sem que ocorra uma diferença de 5% na fitness máxima e mínima
     static int cont{0};
 
@@ -61,6 +71,18 @@ bool interv(double &min, double &max){
         return(true);
     }else{
         return(false);
+    }
+}
+
+void mutate(vector<Tour> &t){
+    for(Tour objT : t){
+        mutation(objT);
+    }
+}
+
+void crossPop(vector<Tour> &t){
+    for(int i=0;i<t.size()/2;i++){
+        crossover(t[rand()%t.size()],t[rand()%t.size()]);
     }
 }
 
